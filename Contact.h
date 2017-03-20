41533bd58b0f39a2a811c46f2081c4bbee2df09e
Jelani Thompson <jelani.thompson@outlook.com>
2017-03-20 10:40:58 -0400

    Update Contact.h

--- Contact.h
+++ Contact.h
@@ -1,28 +1,35 @@
 #ifndef SICT_FRACTION_H
 #define SICT_FRACTION_H
-using namespace communication;
-class Contact {
-		char m_name[21];
-		long long* m_phoneNumbers;
-		int m_noOfPhoneNumbers;
-	public:
-		// TODO: add the default constructor here
+
+namespace communication {
+    class Contact {
+        char m_name[21];
+        long long *m_phoneNumbers;
+        int m_noOfPhoneNumbers;
+    public:
+        // TODO: add the default constructor here
         Contact();
 
-		// TODO: add the constructor with parameters here
-        Contact(char name[21], long long* phoneNumbers, int noOfPhoneNumbers);
+        // TODO: add the constructor with parameters here
+        Contact(char name[21], long long *phoneNumbers, int noOfPhoneNumbers);
 
         // Use the destructor to make sure that there aren't any memory leaks
         ~Contact();
 
-		// TODO: add the display function here
+        // TODO: add the display function here
         void display() const;
 
-		// TODO: add the isEmpty function here
+        // TODO: add the isEmpty function here
         bool isEmpty() const;
 
-	public:
-		Contact(const Contact& other) = delete;
-		Contact& operator=(const Contact& other) = delete;
-	};
+        // Parsing
+        int numdigits(long long n) const;
+
+        std::string getdigits(long long n, int startPosition, int numberOfDigits) const;
+
+    public:
+        Contact(const Contact &other) = delete;
+        Contact &operator=(const Contact &other) = delete;
+    };
+}
 #endif
